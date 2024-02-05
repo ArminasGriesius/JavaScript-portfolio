8f89103fd3259bdede6d7eff48285e52f01962b8
ArminasGriesius <88167077+ArminasGriesius@users.noreply.github.com>
2024-02-05 21:33:41 +0200

    feat: added generated jokes page from API

--- script.js
+++ script.js
@@ -470,7 +470,6 @@ let frames = 0;
 let movingDirection = "right";
 let isMovingRight = false;
 let isMovingLeft = false;
-
 class Player {
   constructor() {
     this.position = {
@@ -717,3 +716,25 @@ addEventListener("keyup", ({ key }) => {
 });
 
 // /Mario
+
+// Jokes
+function generateJoke(jokeType) {
+  if (jokeType === "chuck") {
+    fetch("https://api.chucknorris.io/jokes/random")
+      .then((response) => response.json())
+      .then(
+        (json) => (document.getElementById("jokeText").innerText = json.value)
+      )
+      .catch((error) => console.error(error));
+  } else if (jokeType === "dad") {
+    fetch("https://icanhazdadjoke.com/", {
+      headers: {
+        Accept: "text/plain",
+      },
+    })
+      .then((response) => response.text())
+      .then((text) => (document.getElementById("jokeText").innerText = text))
+      .catch((error) => console.error(error));
+  }
+}
+// /Jokes
